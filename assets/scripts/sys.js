11f952710452bd29a93675593046dd4bc1ac5a7d
KobbyNua <kobby.nua@gmail.com>
2022-09-08 09:43:19 +0000

    everything added

---
+++ assets/scripts/sys.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+	   var elements=document.getElementsByClassName("read-article")
+        console.log(document.getElementsByClassName('video-blog').childNodes)
+	  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  alert("mobile device");
+}else{
+  // false for not mobile device
+  alert("not mobile device");
+}
+}
+
+
